39943892fad5e494b798c052c9671ee2718b2d3f
zNeilXz <neil0917@live.com>
2023-05-17 22:35:48 -0400

    1. Changed request method. 2. Created function to handle adding questions and answers to reuse later for the other questions. 3. added "TODO.md"

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO List
+
+- [ n] Improve the flow of the quiz by fetching all 10 questions at once and completing the round before requesting new data for the next round.
+- [ ] Disable all buttons once a user clicks on an option to prevent multiple clicks.
+- [ ] Keep track of the user's score and display the number of incorrect answers at the end of each round.
+- [ ] Show the correct answer when the user selects an incorrect option.
--- script.js
+++ script.js
@@ -1,102 +1,124 @@
 var xhr = new XMLHttpRequest();
 var url = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
 
-xhr.open("GET", url, true);
+window.addEventListener("load", async function () {
+  var apiUrl =
+    "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
 
-xhr.onload = function () {
-  var popup = document.getElementById("popup");
+  try {
+    var response = await fetch(apiUrl);
+    var data = await response.json();
 
-  if (xhr.status === 200) {
-    var response = JSON.parse(xhr.responseText);
-    var questionContainer = document.getElementById("question-container");
-    var quizForm = document.getElementById("quiz-form");
-    var submitButton = document.getElementById("submit-button");
-
-    if (response.results.length > 0) {
-      console.log(response.results);
-
-      var firstQuestion = response.results[0];
-      var question = firstQuestion.question;
-
-      console.log(response.results[0]["correct_answer"]);
-
-      var answers = firstQuestion.incorrect_answers.concat(
-        firstQuestion.correct_answer
-      );
-
-      // Create the question element
-      var questionElement = document.createElement("p");
-      questionElement.textContent = question;
-
-      // Append question to the container
-      questionContainer.insertBefore(questionElement, quizForm);
-
-      // Create the answer elements
-      answers.forEach(function (answer) {
-        var answerInput = document.createElement("input");
-        answerInput.type = "radio";
-        answerInput.name = "answer";
-        answerInput.value = answer;
-
-        var answerLabel = document.createElement("label");
-        answerLabel.textContent = answer;
-
-        var answerItem = document.createElement("div");
-        answerItem.appendChild(answerInput);
-        answerItem.appendChild(answerLabel);
-
-        // Append answer item to the form
-        quizForm.appendChild(answerItem);
-      });
-
-      // Create the submit button
-      var submitButton = document.createElement("button");
-      submitButton.type = "submit";
-      submitButton.textContent = "Submit";
-
-      // Append submit button to the form
-      quizForm.appendChild(submitButton);
-
-      // Add submit event listener to the form
-      quizForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        var selectedAnswer = document.querySelector(
-          'input[name="answer"]:checked'
-        ).value;
-
-        // Create result object
-        var result = {
-          question: question,
-          selectedAnswer: selectedAnswer,
-        };
-
-        if (result["selectedAnswer"] == response.results[0]["correct_answer"]) {
-
-          console.log("Correct");
-
-          popup.style.display = "block";
-          popup.querySelector("p").textContent = "Correct!";
-          setTimeout(function () {
-            popup.style.display = "none";
-            location.reload();
-          }, 2000);
-
-        } else {
-
-          console.log("Incorrect");
-
-          popup.style.display = "block";
-          popup.querySelector("p").textContent = "Incorrect";
-          setTimeout(function () {
-            popup.style.display = "none";
-          }, 2000);
-        }
-
-        console.log(result);
-      });
-    }
+    handleQuizData(data);
+  } catch (error) {
+    console.log("An error occurred:", error);
   }
-};
+});
 
-xhr.send();
+function createQuestion(question, answers) {
+  console.log(answers);
+
+  var questionContainer = document.getElementById("question-container");
+  var quizForm = document.getElementById("quiz-form");
+
+  // Create the question element
+  var questionElement = document.createElement("div");
+  questionElement.classList.add("question");
+  questionElement.textContent = question;
+
+  // Append question to the container
+  questionContainer.insertBefore(questionElement, quizForm);
+
+  // Create the answer elements
+  answers.forEach(function (answer) {
+    var answerInput = document.createElement("input");
+    answerInput.type = "radio";
+    answerInput.name = "answer";
+    answerInput.value = answer;
+
+    var answerLabel = document.createElement("label");
+    answerLabel.textContent = answer;
+
+    var answerItem = document.createElement("div");
+    answerItem.appendChild(answerInput);
+    answerItem.appendChild(answerLabel);
+
+    // Append answer item to the form
+    quizForm.appendChild(answerItem);
+  });
+}
+
+function handleQuizData(response) {
+  console.log(response);
+
+  var quizForm = document.getElementById("quiz-form");
+  var submitButton = document.getElementById("submit-button");
+
+  if (response.results.length > 0) {
+    console.log(response.results);
+
+    var firstQuestion = response.results[0];
+    var question = firstQuestion.question;
+
+    var answers = firstQuestion.incorrect_answers.concat(
+      firstQuestion.correct_answer
+    );
+
+    createQuestion(question, answers);
+
+    // Create the submit button
+    var submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.textContent = "Submit";
+
+    // Append submit button to the form
+    quizForm.appendChild(submitButton);
+
+    // Add submit event listener to the form
+    quizForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      var selectedAnswer = document.querySelector(
+        'input[name="answer"]:checked'
+      ).value;
+
+      // Create result object
+      var result = {
+        question: question,
+        selectedAnswer: selectedAnswer,
+      };
+
+      if (result["selectedAnswer"] == response.results[0]["correct_answer"]) {
+        //console.log("Correct");
+
+        popup.style.display = "block";
+        popup.querySelector("p").textContent = "Correct!";
+        setTimeout(function () {
+          popup.style.display = "none";
+
+          firstQuestion = response.results[1];
+          question = firstQuestion.question;
+
+          answers = firstQuestion.incorrect_answers.concat(
+            firstQuestion.correct_answer
+          );
+
+          createQuestion(question, answers);
+          
+        }, 2000);
+      } else {
+        //console.log("Incorrect");
+
+        popup.style.display = "block";
+        popup.querySelector("p").textContent = "Incorrect";
+        setTimeout(function () {
+          popup.style.display = "none";
+
+          createQuestion(question, answers);
+        }, 2000);
+      }
+
+      //console.log(result);
+    });
+  }
+}
